44d52e02f05b2d46ad3387a60a70b19c5857d2d1
h-k-c <2605245956@qq.com>
2020-03-26 16:26:00 +0800

    使用了spring cloud的sentinel

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/springcloud/myHandle/CustomerHandler.java
@@ -0,0 +1,10 @@
+package com.springcloud.myHandle;
+
+public class CustomerHandler {
+    public static String handlerException1(){
+         return "test1----------------------";
+    }
+    public static String handlerException2(){
+        return "test2----------------------";
+    }
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/springcloud/myHandle/RateLimitController.java
@@ -0,0 +1,5 @@
+package com.springcloud.myHandle;
+
+public class RateLimitController {
+
+}
